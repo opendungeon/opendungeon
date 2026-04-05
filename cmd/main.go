6d68905e93ae86e35543252846773c0b3e7f3d32
Dylan Sharp <dgstatic@gmail.com>
2026-04-05 13:55:48 -0600

    a* shortest path

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/opendungeon/opendungeon/pkg/grid"
+)
 
 func main() {
 	fmt.Println("Hello, OpenDungeon!")
+
+	hg := grid.NewHex(5, 5)
+
+	path, err := hg.ShortestPath(grid.NewPoint(0, 0), grid.NewPoint(2, 4))
+	if err != nil {
+		panic(err)
+	}
+
+	for _, cell := range path {
+		hg.At(cell.Q, cell.R).Weight = 0
+	}
+
+	fmt.Println(hg.String())
 }
