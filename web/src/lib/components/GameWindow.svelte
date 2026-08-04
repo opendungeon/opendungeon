ca138d0dd72dec49df971f24c3e965a6dd4e3616
ian-shakespeare <ian@shakespeare.dev>
2026-08-03 22:25:11 -0600

    performance debug menu

--- web/src/lib/components/GameWindow.svelte
+++ web/src/lib/components/GameWindow.svelte
@@ -4,14 +4,22 @@
 
   let { game } = $props<{ game: Game }>();
   let canvas = $state();
+  let isDebugging = $state(import.meta.env.DEV);
+  let frameHandle = -1;
+  let frameNumber = $state(0);
+  let frameTime = $state(1);
+  let framesPerSecond = $derived(1000 / frameTime);
+  let lastTime = 0;
 
-  let lastFrame = 0;
   const runGameLoop = () => {
-    window.requestAnimationFrame((currentFrame) => {
-      const dt = currentFrame - lastFrame;
+    frameHandle = window.requestAnimationFrame((t) => {
+      const dt = t - lastTime;
       game.update(dt);
       game.draw();
-      lastFrame = currentFrame;
+
+      frameNumber += 1;
+      frameTime = t - lastTime;
+      lastTime = t;
 
       runGameLoop();
     });
@@ -19,9 +27,18 @@
 
   onMount(() => {
     game.start(canvas).then(runGameLoop);
+
+    window.onbeforeunload = () => {
+      window.cancelAnimationFrame(frameHandle);
+    };
   });
 </script>
 
 <div id="game-window" class="absolute inset-0 overflow-hidden">
   <canvas bind:this={canvas} class="h-full w-full"></canvas>
+  {#if isDebugging}
+    <code class="font-mono z-50 absolute bottom-0 right-0 pointer-events-none text-[#ff0000]">
+      frame: {frameNumber}, frame time: {frameTime.toFixed()}ms, frames per second: {framesPerSecond.toFixed()}
+    </code>
+  {/if}
 </div>
