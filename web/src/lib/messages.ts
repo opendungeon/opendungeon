0963d96fa011cd3b4dc73d817f4bc949cf40624b
ian-shakespeare <ian@shakespeare.dev>
2026-08-30 17:07:52 -0600

    client side json based messages

---
+++ web/src/lib/messages.ts
@@ -0,0 +1,70 @@
+import type { APIRoom } from "$lib/api";
+
+type Header = {
+  id: number;
+  sentAt: number;
+};
+
+export type AckMessage = Header & {
+  type: "ack";
+  promptId: number;
+  accepted: boolean;
+};
+
+export type AnimateMessage = Header & {
+  type: "animate";
+  characterId: number;
+  animationId: string;
+};
+
+export type ChatMessage = Header & {
+  type: "chat";
+  playerId: string;
+  content: string;
+};
+
+export type JoinMessage = Header & {
+  type: "join";
+  playerId: string;
+  playerName: string;
+};
+
+export type LeaveMessage = Header & {
+  type: "leave";
+  playerId: string;
+};
+
+export type LoadLevelMessage = Header & {
+  type: "loadlevel";
+  levelId: string;
+};
+
+export type MoveMessage = Header & {
+  type: "move";
+  characterId: string;
+  x: number;
+  y: number;
+};
+
+export type PingMessage = Header & {
+  type: "ping";
+  playerId: string;
+  x: number;
+  y: number;
+};
+
+export type SyncMessage = Header & {
+  type: "sync";
+  data: APIRoom;
+};
+
+export type Message =
+  | AckMessage
+  | AnimateMessage
+  | ChatMessage
+  | JoinMessage
+  | LeaveMessage
+  | LoadLevelMessage
+  | MoveMessage
+  | PingMessage
+  | SyncMessage;
